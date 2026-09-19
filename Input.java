50432ed2dcac7c08e1d9e8bfc1f457f8b9d3aa47
allanaprestesp-gif <allanaprestesp@gmail.com>
2026-09-19 17:39:58 -0300

    Criei a classe Input para leitura de números

---
+++ Input.java
@@ -0,0 +1,15 @@
+public class Input {
+
+    public static int readInt(String message) {
+
+        while (true) {
+            try {
+                String valor = IO.readln(message);
+                return Integer.parseInt(valor);
+
+            } catch (Exception e) {
+                IO.println("Valor inválido! Digite um número inteiro.");
+            }
+        }
+    }
+}
